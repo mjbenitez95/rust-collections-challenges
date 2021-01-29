8fd69e4357f8ac936ca07d8534dc7eeedb1f484d
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-28 18:45:49 -0800

    implement calculate_mean()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::Rng;
+use std::collections::HashMap;
+
+fn create_random_list(num_integers: u32) -> Vec<i32> {
+    vec![0, 1, 2, 3, 4, 5]
+}
+
+fn calculate_mean(numbers: &Vec<i32>) -> f32 {
+    numbers.iter().sum::<i32>() as f32 / numbers.len() as f32
+}
+
 fn main() {
-    println!("Hello, world!");
+    // given a list of integers, use a vector and return the mean, median, and mode
+    let num_integers = 30;
+    let list_of_integers: Vec<i32> = create_random_list(num_integers);
+    let mean = calculate_mean(&list_of_integers);
+
+    println!("The mean of {:?} is: {}!", list_of_integers, mean);
 }
