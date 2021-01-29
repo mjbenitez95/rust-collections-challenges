adebe908929cf1202316735abd3e6ce79e5896b6
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-28 19:24:00 -0800

    add other problems

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use rand::Rng;
 use std::collections::HashMap;
 
-const NUM_INTEGERS: u32 = 100;
-const LIST_MIN: i32 = 0;
+const NUM_INTEGERS: u32 = 20;
+const LIST_MIN: i32 = -100;
 const LIST_MAX: i32 = 100;
 
 fn create_random_list(num_integers: u32) -> Vec<i32> {
@@ -45,24 +45,43 @@ fn calculate_median(numbers: &Vec<i32>) -> f32 {
 
     if num_elements % 2 == 0 {
         let middle = num_elements / 2;
-        calculate_mean(&vec![
-            numbers[middle - 1],
-            numbers[middle],
-        ])
+        calculate_mean(&vec![numbers[middle - 1], numbers[middle]])
     } else {
         numbers[num_elements / 2] as f32
     }
 }
 
 fn main() {
-    // given a list of integers, use a vector and return the mean, median, and mode
+    /*
+        Given a list of integers, use a vector and return the mean (the average
+        value), median (when sorted, the value in the middle position), and
+        mode (the value that occurs most often; a hash map will be helpful
+        here) of the list
+    */
     let list_of_integers: Vec<i32> = create_random_list(NUM_INTEGERS);
     let mean = calculate_mean(&list_of_integers);
     let mode = calculate_mode(&list_of_integers);
     let median = calculate_median(&list_of_integers);
 
-    println!("With {:?}, we have: ", list_of_integers);
-    println!("  Mean: {}.", mean);
-    println!("  Mode: {}.", mode);
-    println!("  Median: {}.", median);
+    println!("Problem 1:");
+    println!("  {:?}.", list_of_integers);
+    println!("  Mean: {}, Mode: {}, Median: {}.", mean, mode, median);
+
+    println!("");
+
+    /*
+        Convert strings to pig latin. The first consonant of each word is moved
+        to the end of the word and “ay” is added, so “first” becomes “irst-
+        fay.” Words that start with a vowel have “hay” added to the end instead
+        (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8
+        encoding!
+    */
+
+    /*
+        Using a hash map and vectors, create a text interface to allow a user
+        to add employee names to a department in a company. For example, “Add
+        Sally to Engineering” or “Add Amir to Sales.” Then let the user
+        retrieve a list of all people in a department or all people in the
+        company by department, sorted alphabetically.
+    */
 }
